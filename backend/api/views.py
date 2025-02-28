07f85b0dc02167429c9824da8d347691f7706fe8
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-27 19:14:32 -0500

    Added views that I will implement later, this will be the beginning of project

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics, status
 
 # Create your views here.
+class SearchView(generics.RetrieveAPIView):
+    pass
+
+class CategoriesView(generics.RetrieveAPIView):
+    pass
+
+class CategorySearch(generics.RetrieveAPIView):
+    pass
+
+class IngredientView(generics.RetrieveAPIView):
+    pass
